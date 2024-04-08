5d96026d597f781847b361233153402296c53e8f
Kingkongos5 <113205091+Kingkongos5@users.noreply.github.com>
2024-04-08 13:26:20 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 
 const addList = document.querySelector('.plus');
 const main = document.querySelector('.main__container');
+const brush = document.querySelector('.del');
 
 let id = 0;
 let z = 1;
 let keyPressVariable;
+
+brush.addEventListener("dblclick", function del() {
+   document.querySelectorAll('.list').forEach((list) => {
+      if (Number(list.id) % 2 == 0) {
+         list.classList.add('remove')
+      } else {
+         list.classList.add('removeTwo')
+      }
+      setTimeout(function () {
+         list.remove();
+      }, 6900)
+   })
+})
+
 
 addList.addEventListener('click', function () {
    main.insertAdjacentHTML(
@@ -72,8 +87,14 @@ addList.addEventListener('click', function () {
                btn.addEventListener("click", function () {
                   let list = document.getElementById(`${btn.dataset.id}`)
                   if (list) {
-                     btn.classList.toggle('edit')
                      let h1 = document.getElementById(`${btn.dataset.id}text`);
+                     h1.classList.toggle("focus");
+                     if (h1.classList.contains('focus')){
+                        h1.focus();
+                     } else {
+                        h1.blur();
+                     }
+                     btn.classList.toggle('edit')
                      keyPressVariable = function (e) {
                         keyPress(btn.dataset.id, e)
                      };
@@ -87,17 +108,16 @@ addList.addEventListener('click', function () {
             if (btn.dataset.id != e.target.dataset.id) {
                if (btn.classList.contains('edit')) {
                   btn.classList.remove('edit')
-                  let h1 = this.document.getElementById(`${btn.dataset.id}text`);
+                  let h1 = document.getElementById(`${btn.dataset.id}text`);
+                  h1.classList.remove("focus");
                   h1.blur();
-                  console.log(123);
                }
             } else if (e.target.classList.contains('list__button')) {
                if (btn.classList.contains('edit')) {
                   btn.classList.remove('edit')
-                  console.log(123);
-                  let h1 = this.document.getElementById(`${btn.dataset.id}text`);
+                  let h1 = document.getElementById(`${btn.dataset.id}text`);
+                  h1.classList.remove("focus");
                   h1.blur();
-                  console.log(h1);
                }
             }
          })
@@ -110,6 +130,8 @@ addList.addEventListener('click', function () {
 
 document.addEventListener('mousedown', function (e) {
    var el; // Оголошуємо змінну el
+   let posX = e.clientX;
+   let posY = e.clientY;
    z += 1;
    if (e.target.classList.contains('list__title-image')) {
       return
@@ -118,32 +140,44 @@ document.addEventListener('mousedown', function (e) {
    }
    if (e.target.classList.contains('list')) {
       el = e.target;
+      el.classList.add('grab');
       el.style.zIndex = `${z}`
       addEventListener("mousemove", moveHandler); // Додаємо обробник подій
       addEventListener("mouseup", function () {
+         if (el.classList.contains('grab')) {
+            el.classList.remove('grab')
+         }
+         el.style.borderRadius = "25px 25px 25px 25px";
+         el.style.rotate = '0deg'
          removeEventListener("mousemove", moveHandler); // Видаляємо обробник подій
       })
    } else {
       if (e.target.classList.contains('list__hero') || e.target.classList.contains('h1') || e.target.classList.contains('list__button-menu')) {
          el = e.target.parentNode;
          el.style.zIndex = `${z}`
+         el.classList.add('grab');
          addEventListener("mousemove", moveHandler); // Додаємо обробник подій
          addEventListener("mouseup", function () {
-            removeEventListener("mousemove", moveHandler); // Видаляємо обробник подій
-         })
-      } else {
-         el = e.target.parentNode.parentNode;
-         el.style.zIndex = `${z}`
-         addEventListener("mousemove", moveHandler); // Додаємо обробник подій
-         addEventListener("mouseup", function () {
+            if (el.classList.contains('grab')) {
+               el.classList.remove('grab')
+            }
+            el.style.borderRadius = "25px 25px 25px 25px";
+            el.style.rotate = '0deg'
             removeEventListener("mousemove", moveHandler); // Видаляємо обробник подій
          })
       }
    }
 
    function moveHandler(e) {
-      el.style.left = (e.clientX - (el.offsetWidth / 2)) + 'px'; // Встановлюємо стиль left
-      el.style.top = (e.clientY - el.offsetHeight) + 'px'; // Встановлюємо стиль left
+      if (posX < e.clientX) {
+         el.style.borderRadius = `25px 25px ${((e.clientX - posX) / 15) + 25}px 25px`;
+         el.style.rotate = `${(e.clientX - posX) / 35}deg`;
+      } else {
+         el.style.borderRadius = `25px 25px 25px ${((posX - e.clientX) / 10) + 25}px`;
+         el.style.rotate = `${(e.clientX - posX) / 35}deg`;
+      }
+      el.style.left = (e.clientX - (el.offsetWidth / 1.75)) + 'px'; // Встановлюємо стиль left
+      el.style.top = (e.clientY - (el.offsetHeight / 1.5)) + 'px'; // Встановлюємо стиль left
    }
 });
 
@@ -153,6 +187,9 @@ function keyPress(id, e) {
       if ((btn.dataset.id == id) && btn.classList.contains("edit")) {
          h1.focus();
          if (e.key == "Enter") {
+            if (h1.classList.contains('focus')) {
+               h1.classList.remove("focus");
+            }
             btn.classList.remove("edit");
             h1.blur();
          }
